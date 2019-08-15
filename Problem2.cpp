d7de9d0b2774978a776cb4a5f6997a883e15191f
Shazmaan <shazmaan@buffalo.edu>
2019-08-14 21:30:11 -0400

    changed mid for Problem2.

--- Problem2.cpp
+++ Problem2.cpp
@@ -13,7 +13,7 @@ public:
     vector<int> findClosestElements(vector<int>& A, int k, int x) {
         int left = 0, right = A.size() - k;
         while (left < right) {
-            int mid = (left + right) / 2;
+            int mid = left - (left+right)/2;
             if (x - A[mid] > A[mid + k] - x)
                 left = mid + 1;
             else
