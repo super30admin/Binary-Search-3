97c7a8e886620249419e2027eca02ba794268960
Mudita Bysani <muditabysani@muditas-mbp.home>
2019-09-11 22:25:24 -0700

    Finished 1&2 using binary search

--- Problem1.py
+++ Problem1.py
@@ -5,6 +5,7 @@ class Solution(object):
 		:type n: int
 		:rtype: float
 		"""
+		# what is the time complexity of this?
 		return x ** n
 
 	def myPow(self, x, n):
@@ -25,3 +26,18 @@ class Solution(object):
 		if isNeg:
 			res = 1/res
 		return res
+
+	def myPow(self, x: float, n: int) -> float:
+		# Time Complexity : O(logn) because we are dividing the value of n into half
+		# When the value of n is even we just return temp^2
+		# If its odd we have to check for the n value if n < 0 then return temp^2*1/x else return temp^2
+		if n == 0:
+			return 1
+		temp = self.myPow(x, int(n/2))
+		if n%2 == 0:
+			return temp * temp
+		else:
+			if n > 0:
+				return temp * temp * x
+			else:
+				return temp * temp * 1/x
--- Problem2.py
+++ Problem2.py
@@ -13,3 +13,26 @@ class Solution(object):
 		res = res[:k]
 		res.sort()
 		return res
+
+	def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
+		# Time Complexity : O(n - k) because we don't check the k elements that lie between our low and high pointer
+		low = 0
+		high = len(arr) - 1
+		while high - low + 1 > k:
+			if abs(arr[low] - x) > abs(arr[high] - x):
+				low += 1
+			else:
+				high -= 1
+		return arr[low:high + 1]
+
+	def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
+		# Time Complexity : O(log(n - k)) because we use binary search to find the correct position of our low pointer and initially the right pointer is place at len(arr) - k because we have to verify only the n - k elements
+		low = 0
+		high = len(arr) - k
+		while low < high:
+			mid = low + (high - low)//2
+			if abs(arr[mid] - x) > abs(arr[mid + k] - x): # Here we check the mid and mid + k because our correct position for low pointer will be in between mid and min + k locations
+				low = mid + 1
+			else:
+				high = mid
+		return arr[low:low + k]
