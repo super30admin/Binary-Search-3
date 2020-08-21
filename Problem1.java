636d0e6ee1f5d3db2fb271947616cb4f398ccfc6
soumil.sarvaiya <ss8165@rit.edu>
2020-08-21 14:12:56 -0400

    Completed Problem1

--- Problem1.java
+++ Problem1.java
@@ -1,7 +1,35 @@
-// Time Complexity :
-// Space Complexity :
-// Did this code successfully run on Leetcode :
-// Any problem you faced while coding this :
+// Time Complexity : O(log n)
+//       n: exponent
+// Space Complexity : O(log n)
+//       n: exponent
+//     Recursive stack space
+// Did this code successfully run on Leetcode : yes
+// Any problem you faced while coding this : no
 
 
 // Your code here along with comments explaining your approach
+class Problem1 {
+    public double myPow(double x, int n) {
+        
+        // base case
+        if(n==0)
+            return 1;
+        
+        // recursively call myPow ->n/2
+        double result = myPow(x, n/2);
+        
+        // multiply result
+        result *= result;
+        // check odd 
+        if (n%2 != 0){ 
+            // check n negative
+            if(n < 0)
+               result *= (1/x);
+            // check n positive
+            else
+               result *= x;
+        }
+        // return result
+        return result; 
+    }
+}
