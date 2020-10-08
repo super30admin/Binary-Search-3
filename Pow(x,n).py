1fd7e4275c905f76cf2572b10ef771c6a80259fc
Rohit gorle <61993839+rohit-gorle@users.noreply.github.com>
2020-10-08 04:36:02 -0700

    recursion

---
+++ Pow(x,n).py
@@ -0,0 +1,17 @@
+Time - O(logn)
+SPace- recursion stack
+
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        
+        if n == 0: 
+            return 1
+        if n < 0:
+            return 1/self.myPow(x, -n)
+        
+        lower = self.myPow(x, n//2)
+        if n % 2 == 0:
+            return lower*lower
+        else: 
+            return lower*lower*x
+    
