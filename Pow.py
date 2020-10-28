418f66bf0629e3dcf6656f1bad183d01bb055748
Piyush Kurkure <piyushk10@gmail.com>
2020-10-27 23:39:31 -0700

    Tried BS-3

---
+++ Pow.py
@@ -0,0 +1,22 @@
+# TC: O(Logn)
+# SC: O(1)
+# Yes, It ran on leetcode
+# No problems faced
+
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        
+        def helper(x,n):
+            if n==1:
+                return 2
+            mid = helper(x,n//2)
+            if n%2==0:
+                return mid*mid
+            else:
+                return mid*mid*2
+            
+            
+        if n<0:
+            return 1/helper(x,n)
+        else:
+            return helper(x,n)
---
+++ findClosestElements.py
@@ -0,0 +1,8 @@
+# TC: Not able to solve this question
+# SC: Not able to solve this question
+
+# class Solution(object):
+#     def findClosestElements(self, arr, k, x):
+#         left, right = 0, len(A) - k
+#         while left < right:
+#             mid = (left + right) / 2
