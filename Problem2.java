4b40a75c0adb456fec14e3b299799d65b93c2f03
koshyanand <koshyanand@gmail.com>
2020-10-28 18:44:56 -0700

    Completed 1

---
+++ Problem2.java
@@ -0,0 +1,51 @@
+// Time Complexity: O(max(log(N), K))
+// Space Complexity: O(max(log(N), K))
+// Passed Leetcode
+
+import java.lang.*;
+
+class Solution {
+    int x;
+    int[] arr;
+    public int binarySearch(int low, int high) {
+        
+        if (high - low == 0) {
+            return high;
+        }
+        
+        int mid = low + (high - low) / 2;
+        
+        if (arr[mid] > x) {
+            return binarySearch(low, mid);
+        } else {
+            return binarySearch(mid + 1, high);
+        }
+    }
+    
+    
+    public void slice(LinkedList curr, int low, int high) {
+        for (int i = low; i < high; i++) {
+            curr.add(arr[i]);
+        }
+    }
+    
+    public List<Integer> findClosestElements(int[] arr, int k, int x) {
+        
+        this.x = x;
+        this.arr = arr;
+        int index = binarySearch(0, arr.length - 1);
+        
+        LinkedList<Integer> curr = new LinkedList<>();
+        slice(curr, Math.max(index - k, 0), Math.min(index + k + 1, arr.length));
+        while (curr.size() > k) {
+
+            if (Math.abs(curr.getFirst().intValue() - x) == Math.abs(curr.getLast().intValue() - x) || Math.abs(curr.getFirst().intValue() - x) < Math.abs(curr.getLast().intValue() - x))
+                curr.removeLast();
+            else curr.removeFirst();
+
+        }
+        return curr;
+
+        
+    }
+}
