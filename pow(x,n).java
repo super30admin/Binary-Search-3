546581b262727ddc09b48ad20f10edd178b148fc
Rishi Raj <rishiraj824@gmail.com>
2021-01-20 20:30:52 -0500

    Create pow(x,n).java

---
+++ pow(x,n).java
@@ -0,0 +1,20 @@
+// Time - O(lgN)
+// space - O(1)
+class Solution {
+    public double myPow(double x, int n) {
+        
+        if( n == 0) {
+            return 1;
+        }
+        double temp = myPow (x, n / 2);
+        if(n%2==0) {
+            return temp * temp;
+        }
+        else {
+            if(n < 0) {
+                return  temp * temp * 1 / x;
+            }
+            return temp * temp * x;
+        }
+    }
+}
