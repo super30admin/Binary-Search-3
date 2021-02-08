593f5a7e5c047c4158f5f30e8a377d5d9f1faa80
Samruddhi Landge <samruddhi.landge@sjsu.edu>
2021-02-08 15:20:47 -0800

    updated powx-n

--- 50-powx-n.java
+++ 50-powx-n.java
@@ -1,12 +1,16 @@
 /**LC-50
  * Time Complexity : O(logN) 
- * Space Complexity :
+ * Space Complexity : O(1)
  * Did this code successfully run on Leetcode : Yes
  * Any problem you faced while coding this : No
  *
  Algorithm:
- 1. 
- 
+ Here we reduce the search space by half by reducing value of N into a half each time.
+ We continue this till n becomes 0. 
+ Gist: 
+ Even N: eg 3^6 = 3^3 * 3^3
+ Odd N: 3^ 7 = 3^3 * 3^3 * 3
+ In case of negative, in Odd, we do (1/3)
  */
 class Solution {
     public double myPow(double x, int n) {
