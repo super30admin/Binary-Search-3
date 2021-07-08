425cc81220e1708605e1f8504f222bb80c2ee88e
Sumit Chavan <chavansumit13@gmail.com>
2021-07-08 21:13:08 +0530

    Completed Sprint Binary-Search-3

--- KClosestElements.java
+++ KClosestElements.java
@@ -5,6 +5,21 @@ import java.util.PriorityQueue;
 
 public class KClosestElements {
 
+    /*
+    TC : O(N + klogk)
+    SC : O(N)
+    leetcode : yes
+    problems faced : No
+     */
+
+    /**
+     * Use a min heap and all elements in the heap and remove top k elements and sort them at the end
+     *
+     * @param arr
+     * @param k
+     * @param x
+     * @return
+     */
     public List<Integer> findClosestElements(int[] arr, int k, int x) {
         PriorityQueue<Integer> minHeap = new PriorityQueue<>(k, (a, b) -> Math.abs(a - x) == Math.abs(b - x) ? a - b : Math.abs(a - x) - Math.abs(b - x));
 
@@ -22,44 +37,73 @@ public class KClosestElements {
     }
 
 
+    public List<Integer> findClosestElements_BinarySearch(int[] arr, int k, int x) {
+        List<Integer> result = new ArrayList<Integer>();
+
+        int low = 0;
+        int high = arr.length - k;
+
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+
+            int distF = x - arr[mid];
+            int distL = arr[mid + k] - x;
+
+            if (distF > distL)
+                low = mid + 1;
+            else
+                high = mid;
+
+
+        }
+
+        for (int i = low; i < low + k; i++) {
+            result.add(arr[i]);
+        }
+
+        return result;
+    }
+
+
     class Solution {
         public List<Integer> findClosestElements(int[] arr, int k, int x) {
-            int left = BinarySearch (arr, x, 0, arr.length - 1);
+            int left = BinarySearch(arr, x, 0, arr.length - 1);
             int right = left + 1;
-            List <Integer> list = new ArrayList ();
+            List<Integer> list = new ArrayList();
             int count = 0;
             while (left >= 0 && right < arr.length && count < k) {
-                if (x - arr [left] <= arr [right] - x)
-                    list.add (arr [left--]);
+                if (x - arr[left] <= arr[right] - x)
+                    list.add(arr[left--]);
                 else
-                    list.add (arr [right++]);
+                    list.add(arr[right++]);
                 count++;
             }
             while (count < k && left >= 0) {
-                list.add (arr [left--]);
+                list.add(arr[left--]);
                 count++;
             }
             while (count < k && right < arr.length) {
-                list.add (arr [right++]);
+                list.add(arr[right++]);
                 count++;
             }
-            Collections.sort (list);
+            Collections.sort(list);
             return list;
         }
-        public int BinarySearch (int arr [], int key, int left, int right) {
-            if (key <= arr [left])
+
+        public int BinarySearch(int arr[], int key, int left, int right) {
+            if (key <= arr[left])
                 return left;
-            if (key >= arr [right])
+            if (key >= arr[right])
                 return right;
             int mid = left + (right - left) / 2;
-            if (mid < arr.length - 1 && key >= arr [mid] && key < arr [mid + 1])
+            if (mid < arr.length - 1 && key >= arr[mid] && key < arr[mid + 1])
                 return mid;
-            if (mid > 0 && key < arr [mid] && key >= arr [mid - 1])
+            if (mid > 0 && key < arr[mid] && key >= arr[mid - 1])
                 return mid - 1;
-            if (key < arr [mid])
-                return BinarySearch (arr, key, left, mid - 1);
+            if (key < arr[mid])
+                return BinarySearch(arr, key, left, mid - 1);
             else
-                return BinarySearch (arr, key, mid + 1, right);
+                return BinarySearch(arr, key, mid + 1, right);
         }
     }
 }
