aade1e9988405995b46805327f9c96542d3c20fb
DaryaniLeena <daryani.l@husky.neu.edu>
2021-08-04 23:29:38 -0700

    added sol II for K closest elements

--- KClosestElementsOfX.java
+++ KClosestElementsOfX.java
@@ -24,3 +24,31 @@ class Solution {
         return result;
     }
 }
+
+//Solution 2
+//Time Complexity:O(n) ; n is no of elements in array
+//Space Complexity: O(1);
+class Solution {
+    public List<Integer> findClosestElements(int[] arr, int k, int x) {
+        if(arr == null || arr.length == 0){
+            return new ArrayList<>();
+        }
+
+        int low =0,high= arr.length - 1;
+        while(high-low+1 > k){
+
+            int lowdist = Math.abs(arr[low] - x);
+            int highdist = Math.abs(arr[high] - x);
+            if(lowdist>highdist){
+                low++;
+            }else{
+                high--;
+            }
+        }
+        List<Integer> result = new ArrayList<>();
+        for(int i=low;i<low+k;i++){
+            result.add(arr[i]);
+        }
+        return result;
+    }
+}
