ae0a932740d67f4b01aec38fedf2cd4f0101897d
VedantMahajan99 <themahajan99@gmail.com>
2021-08-15 20:39:36 -0500

    binary search 3 complete

---
+++ Pow(x, n).java
@@ -0,0 +1,28 @@
+class Solution {
+
+    public double myPow(double x, int n) {
+
+        if(n==0) return 1.0;
+
+        double res = myPow(x,n/2);
+
+
+        if(n%2==0)
+        {
+            return res*res;
+        }
+        else
+        {
+           if(n>0)
+            {
+                return res*res*x;
+            }
+            else
+            {
+                return res*res*(1/x);
+            }
+        }
+
+
+    }
+}
