ba56eda3c4ab676a813affb4cd1823fad2530be4
Durgesh Patil <patildurgesh1998@gmail.com>
2021-09-04 15:54:23 -0400

    Update Problem-2.cpp

--- Problem-2.cpp
+++ Problem-2.cpp
@@ -22,3 +22,29 @@ public:
         return result;
     }
 };
+
+/*
+Time Complexity = O(log(n-k))
+Space Complexity = O(1)
+where N is the number of elements in array.
+*/
+class Solution {
+public:
+    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+        int l = 0 , r = arr.size()-k;
+        while(l<r)
+        {
+            int mid = l+(r-l)/2;
+            int distl = x - arr[mid];
+            int distr = arr[mid+k] - x;
+            if(distl>distr)
+                l = mid+1;
+            else
+                r = mid;
+        }
+        vector<int> result;
+        for(int i=l;i<r+k;i++)
+            result.push_back(arr[i]);
+        return result;
+    }
+};
