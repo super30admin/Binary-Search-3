3022b7d6388adf2ce052e6809945ab66058bc9ae
Tanvie Kirane <tanvie.kirane@gmail.com>
2021-09-29 15:37:54 -0400

    Completed Sprint Binary-Search-3

---
+++ powX.java
@@ -0,0 +1,19 @@
+// Time Complexity : O(log n)
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this :
+
+class Solution {
+    public double myPow(double x, int n) {
+        // base
+        if(n == 0) return 1.00;
+        // logic
+        double result = myPow(x, n/2);
+        if(n%2 == 0){
+            return result * result;
+        } else{
+            if(n > 0) return result * result * x;
+            else return result * result * (1/x);
+        }
+    }
+}
