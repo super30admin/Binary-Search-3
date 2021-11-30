e0f70b3fa66503cdf7a6b2966227a40ad3062cb7
Adhish Shrivastava <adhish70@gmail.com>
2021-11-30 13:05:13 -0500

    Problem87 done

---
+++ Problem-88 Find K Closest Elements.py
@@ -0,0 +1,11 @@
+# 658. Find K Closest Elements
+# https://leetcode.com/problems/find-k-closest-elements/
+
+# Logic:
+
+# Time Complexity:
+# Space Complexity: 
+
+class Solution:
+    def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
+        pass
