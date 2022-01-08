71abc869ff9cffa9e63d15831137b23816117e57
Ikshaku Goswami <ig752994@gmail.com>
2022-01-08 19:43:03 +0530

    completed

--- sample.java
+++ sample.java
@@ -21,3 +21,47 @@ class Solution {
         }
     }
 }
+
+// k closest elements
+
+//tc log(n-k)
+//sc k
+class Solution {
+    public List<Integer> findClosestElements(int[] arr, int k, int x) {
+        
+        List<Integer> result = new ArrayList<>();
+        int low = 0;
+//         int high = arr.length - 1;
+        
+//         while(high - low + 1 > k){   //logn + k algo
+            
+//             int distL = Math.abs(arr[low] - x);
+//             int distR = Math.abs(arr[high] - x);
+            
+//             if(distL > distR) low++;
+            
+//             else
+//                 high--;
+//         }
+        
+        int high = arr.length - k;
+        
+        while(low < high) {
+            
+            int mid = low + (high-low)/2;
+            
+            int distS = x - arr[mid]; 
+            int distL = arr[mid + k] - x;
+            
+            if(distS > distL) low = mid + 1;
+            
+            else high = mid;
+        }
+        
+        for(int i = low; i < low + k; i++){
+            
+            result.add(arr[i]);
+        }
+        return result;
+    }
+}
