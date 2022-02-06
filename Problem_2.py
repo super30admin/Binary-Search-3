f9bb6c43f6861d3bde0efee96cdb0a61c7718094
Piyush Manglani <piyushmanglani@Piyushs-MacBook-Pro.local>
2022-02-06 18:40:36 -0500

    Optimized

--- Problem_2.py
+++ Problem_2.py
@@ -1,30 +1,15 @@
-#Time comlpexity Log(n) + k
-# Space complexity O(1) not using any extra array so space is constant
-# runs on leetcode = YES
-# problems while doing this = No
+# time complexity - log(n - k) where k is the given numbers to find
+# Space Complexity - O(k) 
+# runs on leetcode : YESS
+# Any Prolems : No
 class Solution:
     def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
-        l, r = 0, len(arr) - 1
-        
-        # Find index of x or the closest val to x
-        val, idx = arr[0], 0
-        while l <= r:
-            m = (l + r) // 2
-            curDiff, resDiff = abs(arr[m] - x), abs(val - x)
-            if (curDiff < resDiff or 
-                (curDiff == resDiff and arr[m] < val)):
-                val, idx = arr[m], m
-
-            if arr[m] < x:    l = m + 1
-            elif arr[m] > x:  r = m - 1
-            else:             break
-    
-        l = r = idx                
-        for i in range(k - 1):
-            if l == 0:           
-                r += 1
-            elif r == len(arr) - 1 or x - arr[l-1] <= arr[r+1] - x:               
-                l -= 1
-            else:
-                r += 1        
-        return arr[l:r+1]
+        n = len(arr)
+        low, high = 0, n-k
+        while (low < high):
+            mid = low+(high - low)//2
+            distS = x - arr[mid]
+            distL = arr[mid+k] - x
+            if distS > distL: low = mid +1
+            else: high = mid
+        return arr[low:low+k]
