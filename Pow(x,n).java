b6da255a986bbe7af0ec709aec37fb73cc0c0819
Mounika <90787182+bmounikareddy98@users.noreply.github.com>
2022-02-05 18:56:10 -0600

    Add files via upload

---
+++ Pow(x,n).java
@@ -0,0 +1,24 @@
+//Time complexity: O(logn)
+//Space complexity: O(1)
+
+class Solution {
+    public double myPow(double x, int n) {
+        //base
+        if(n==0)
+            return 1;
+        //logic
+        
+       
+       double temp = myPow(x, n/2);
+        if(n%2==0)
+            return temp*temp;
+        else
+        {
+            if(n>0)
+            return temp*temp*x;
+            else
+            return temp*temp*(1/x);
+        }
+    }
+    
+}
