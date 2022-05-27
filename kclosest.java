541125fbd61b5ea7c9b0c8e238cd32eb00b111ec
r-ashwini <34915478+r-ashwini@users.noreply.github.com>
2022-05-27 00:11:27 -0700

    Update kclosest.java

--- kclosest.java
+++ kclosest.java
@@ -1,3 +1,36 @@
+// binary search on a window ,  O(\log(N - k) + k)
+class Solution {
+    public List<Integer> findClosestElements(int[] arr, int k, int x) {
+        // Initialize binary search bounds
+        int left = 0;
+        int right = arr.length - k;
+        int mid =0;
+        
+        // Binary search against the criteria described
+        while (left < right) {
+            mid = (left + right) / 2;
+            if (x - arr[mid] > arr[mid + k] - x) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        
+        // Create output in correct format
+        List<Integer> result = new ArrayList<Integer>();
+        for (int i = left; i < left + k; i++) {
+            result.add(arr[i]);
+        }
+        
+        return result;
+    }
+}
+
+
+
+
+/* 
+
 // Two pointer solution TC O(n) and SC O(1)
 // S
 class Solution {
@@ -26,3 +59,4 @@ class Solution {
     }
 
 }
+*/
