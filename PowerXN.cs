2b51396d9ea74f3c4e3e20e5a2824ed5a18ccd00
Rincy Soman <rincysoman@gmail.com>
2022-06-09 23:20:06 -0700

    PowerXn

---
+++ PowerXN.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace Algorithms
+{
+    public class PowerXN
+    {
+        /// Time Complexity : O(Logn)
+        // Space Complexity :O(n)
+        // Did this code successfully run on Leetcode :Yes
+        // Any problem you faced while coding this :  No
+        public double MyPow(double x, int n)
+        {
+            //base
+            if (n == 0) return 1.0000;
+            double result = MyPow(x, n / 2);
+            if (n % 2 == 0)
+            {
+                result = result * result;
+            }
+            else
+            {
+                if (n < 0)
+                {
+                    result = result * result * 1 / x;
+                }
+                else
+                {
+                    result = result * result * x;
+                }
+            }
+            return result;
+        }
+    }
+}
