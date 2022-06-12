d2d8a7dacf30c8e25901c3cdf3e2219f97a7ce59
Abhishek Chaudhary <abhishek.chaudhary.2812@gmail.com>
2022-06-12 15:39:07 -0500

    Completed Binary Search 3

---
+++ FindKClosestFirst.java
@@ -0,0 +1,32 @@
+class Solution 
+{
+    public List<Integer> findClosestElements(int[] arr, int k, int x) 
+    {
+        List<Integer> output = new ArrayList<>();
+        
+        if(arr == null || arr.length ==0)
+            return output;
+        
+        int low = 0;
+        int high = arr.length -1;
+        
+        while(high-low + 1 > k)
+        {
+            int diffLow = Math.abs(arr[low]-x);
+            int diffHigh = Math.abs(arr[high]-x);
+            
+            if(diffLow <= diffHigh)
+                high--;
+            else
+                low++;
+        }
+        
+        for(int i = low; i <=high; i++)
+        {
+            output.add(arr[i]);
+        }
+        
+        return output;
+        
+    }
+}
