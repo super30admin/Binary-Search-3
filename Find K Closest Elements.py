36a09df8cff84b132108dfeb10272b1e65d9bb39
Gaurav Tyagi <gauravtyagi777@gmail.com>
2022-07-09 11:50:53 -0500

    Create  Find K Closest Elements.py

---
+++ Find K Closest Elements.py
@@ -0,0 +1,16 @@
+"""
+TC/SC: O(log n)/O(1)
+"""
+class Solution:
+    def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
+        l = 0 
+        r = len(arr)-k
+        
+        while l < r:
+            mid = (l + r) //2
+            
+            if x - arr[mid] > arr[mid+k] - x :
+                l = mid + 1
+            else:
+                r = mid
+        return arr[l:l + k]     
