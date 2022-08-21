b0a33f4087d144c7594c1d1a848582c390185f90
siddbanerjee2012 <sidd.banerjee2012@gmail.com>
2022-08-21 14:27:22 -0500

    Completed Sprint Binary Search-3

---
+++ k-closest_elements.java
@@ -0,0 +1,32 @@
+// Time Complexity : O(n-k)
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
+
+
+// Your code here along with comments explaining your approach
+class Solution {
+    public List<Integer> findClosestElements(int[] arr, int k, int x) {
+        if(arr == null || arr.length == 0){
+            return new ArrayList<>();
+        }
+        int left = 0;
+        int right = arr.length-1;
+
+        while(right - left  + 1 > k){
+            int distLeft = x - arr[left];
+            int distRight = arr[right] - x;
+            if(distLeft > distRight){
+                left++;
+            }
+            else{
+                right--;
+            }
+        }
+        List<Integer> result = new ArrayList<>();
+        for(int i=left; i<= right;i++){
+            result.add(arr[i]);
+        }
+        return result;
+    }
+}
