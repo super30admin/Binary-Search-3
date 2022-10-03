e0bcecd7379cf90cbeab540ec67be9c590598b15
kbadani <badani.kashyap7@gmail.com>
2022-10-03 19:43:20 -0400

    Create PowerX.java

---
+++ PowerX.java
@@ -0,0 +1,22 @@
+/*
+Time Complexity - O(log(n))
+Space Complexity - O(log(n))
+*/
+
+class Solution {
+    public double myPow(double x, int n) {
+        
+        if(n==0) return 1.00;
+        
+        double result = myPow(x,n/2);
+        
+        if(n%2==0){
+            return result * result;  
+        }
+        else{
+            if (n>0) return result * result * x; 
+            else return result *result * 1/x;
+        }
+        
+    }
+}
