017b036edbd76c422ff417a2846c4a154f18952a
Vedant Nibandhe <vnibandhe@gmail.com>
2022-10-17 02:56:46 -0500

    Completed Sprint Binary-Search-3

---
+++ findKClosest.py
@@ -0,0 +1,16 @@
+#Time Complexity: O(log(n-k)) 
+#Space Complexity: O(1)
+#Did it run on leetcode: Yes
+
+class Solution:
+    def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
+        left = 0
+        right = len(arr) - k
+        while left < right:
+            mid = (left + right) // 2
+            if x - arr[mid] > arr[mid + k] - x:
+                left = mid + 1
+            else:
+                right = mid
+
+        return arr[left:left + k]
---
+++ pown.py
@@ -0,0 +1,25 @@
+#Time Complexity: O(logn) 
+#Space Complexity: O(1)
+#Did it run on leetcode: Yes
+
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        
+        if n == 0: return 1
+        nege = False
+        
+        if n < 0: 
+            nege = True
+            n *= -1
+        
+        res = 1
+        while n:
+            if n % 2:
+                res *= x
+                n -= 1
+            else:
+                x *= x
+                n = n//2
+                
+        if nege: return 1/res
+        return res
