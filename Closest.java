3e9b8b1c085140debb3bb5896a7ff631c974a947
Shivani31996 <shivani.chavan96@gmail.com>
2022-11-11 14:59:32 -0500

    Adding different approaches

--- Closest.java
+++ Closest.java
@@ -5,7 +5,9 @@
 //high at the last element, we are placing it on (n-k)th element.
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.PriorityQueue;
 
 public class Closest {
     public static List<Integer> findClosestElements(int[] arr, int k, int x) {
@@ -45,5 +47,73 @@ public class Closest {
         {
             System.out.println(print.get(i));
         }
+    }
+}
+
+/*
+ * Approach 2 - Using a Custom comparator in Max Heap Solution
+ * TC = O(klogk + nlogk)
+ */
+class Solution {
+    public List<Integer> findClosestElements(int[] arr, int k, int x) {
+        PriorityQueue<int []> pq = new PriorityQueue<>((a,b) ->
+            {
+               if(a[1] == b[1])
+               {
+                   return b[0] - a[0];
+               }
+                return b[1] - a[1];
+            });
+        
+        for(int i = 0; i<arr.length;i++)
+        {
+            pq.add(new int[]{arr[i],Math.abs(arr[i] - x)});
+            if(pq.size() > k)
+            {
+                pq.poll();
+            }
+        }
+        
+        List<Integer> result = new ArrayList<>();
+        while(!pq.isEmpty())
+        {
+            int[] pair = pq.poll();
+            result.add(pair[0]);
+        }
+        Collections.sort(result);
+        return result;
+    }
+}
+
+/*
+ * Approach 3: 2 pointer method
+ * TC - O(n-k)
+ * 1) Have two pointers one at the first element, one at the last element.
+ * 2)Calculate the abs distance between the elements and check which of the distances is greater than other and the pointers inwards equally.
+ * 3)Continue this loop until the distance between both the pointers is n-k
+ */
+class Solution {
+    public List<Integer> findClosestElements(int[] arr, int k, int x) {
+        int n = arr.length;
+        int l = 0;
+        int r = n -1;
+        
+        while(r-l+1 > k)
+        {
+            if(Math.abs(arr[l] - x) > Math.abs(arr[r] - x))
+            {
+                l++;
+            }
+            else
+            {
+            r--;}
+        }
+        
+        List<Integer> result = new ArrayList<>();
+        for(int i =l;i<=r;i++)
+        {
+            result.add(arr[i]);
+        }
+        return result;
     }
 }
