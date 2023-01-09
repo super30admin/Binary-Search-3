edfeafabe5c307c681a7059255787d45a9e1aa41
Arbind Singh <getarbind@gmail.com>
2023-01-09 00:12:56 -0500

    commit1

---
+++ Powxn.java
@@ -0,0 +1,21 @@
+//Time Complexity :  O(log n)
+//Space Complexity :  O(n/2)
+//Did this code successfully run on Leetcode : Yes
+
+class Solution {
+    public double myPow(double x, int n) {
+        //base
+        if (n==0) return 1;
+        //logic
+        double result = myPow(x, n/2);
+        if (n%2 == 0)
+            return result *result;
+        else {
+            if (n<0){
+                return result *result*1/x;
+            } else {
+                return result*result*x;
+            }
+        }
+    }
+}
