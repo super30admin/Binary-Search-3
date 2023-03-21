2e37b9711421b0b49cb47d2a276d86711d069aa5
dreamypranjali <41265376+dreamypranjali@users.noreply.github.com>
2023-03-21 16:54:38 -0700

    BS-3 programs

---
+++ Find K Closest Elements.java
@@ -0,0 +1,54 @@
+class Solution {
+    public List<Integer> findClosestElements(int[] arr, int k, int x) {
+        List<Integer> result = new ArrayList<Integer>();
+        
+        
+        if (arr.length == k) {
+            for (int i = 0; i < k; i++) {
+                result.add(arr[i]);
+            }
+            
+            return result;
+        }
+        
+        
+        int left = 0;
+        int right = arr.length;
+        int mid = 0;
+        while (left < right) {
+            mid = (left + right) / 2;
+            if (arr[mid] >= x) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        
+        
+        left -= 1;
+        right = left + 1;
+        
+        
+        while (right - left - 1 < k) {
+            // Be careful to not go out of bounds
+            if (left == -1) {
+                right += 1;
+                continue;
+            }
+            
+            
+            if (right == arr.length || Math.abs(arr[left] - x) <= Math.abs(arr[right] - x)) {
+                left -= 1;
+            } else {
+                right += 1;
+            }
+        } 
+
+        // Build and return the window
+        for (int i = left + 1; i < right; i++) {
+            result.add(arr[i]);
+        }
+        
+        return result;
+    }
+}
---
+++ Pow(x, n).java
@@ -0,0 +1,42 @@
+class Solution {
+	//O(Time): log n
+	//O(Space): log n
+    public double myPow(double x, int n) {
+        //This is a good question for practicing recursion.
+        //Every time we divide the power by 2, by doing this the power needs to be calculated only log n times.
+        //go all the way down till quotient = 0
+        
+        //Handle the negative case.
+        long N = n;
+        if(N < 0){
+            x = 1/x;
+            N = -N;
+        }
+        
+        //No need to send remainder, you can remove it later.
+        return  pow_helper(x, N, 1234);
+         
+    }
+    
+    public double pow_helper(double base, long q, long rem){
+        
+        if(q == 0){
+            
+            return 1.0;
+            
+        }
+        long remainder = q % 2;
+        q = q/2;
+        
+        double to_be_squared = pow_helper(base, q, remainder);
+        
+        if(remainder == 0){
+                return to_be_squared * to_be_squared;
+            }
+            
+            
+        
+        return to_be_squared * to_be_squared * base;
+        
+    }
+}
