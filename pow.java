b59d1d214d56ee110c01d8eb1216f8a3cbbf9386
sivakami-projects <126191544+sivakami-projects@users.noreply.github.com>
2023-04-20 08:02:33 -0700

    Update pow.java

--- pow.java
+++ pow.java
@@ -1,1 +1,19 @@
-
+/*
+ Time Complexity - O (log n) 
+ Space Complexity - O(log n) where log n is the size of the stack space used for recursive call.
+                                   n is the input value n.
+ */
+class Solution {
+    public double myPow(double x, int n) {
+        if(n == 0)  return 1;
+        double temp = myPow(x, n/2);
+        
+        if(n % 2 == 0)
+            return temp*temp;
+        else
+            if(n > 0)
+                return temp*temp*x;
+            else
+                return (temp*temp)/x; 
+    }
+}
