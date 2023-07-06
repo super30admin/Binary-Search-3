231b3b8cd777319a8c185bacb5691997550e3640
jackpraveenraj <jackpraveenraj.work@gmail.com>
2023-07-05 19:21:27 -0700

    problem 1 solved

--- problem1.cpp
+++ problem1.cpp
@@ -8,16 +8,17 @@ Any problem you faced while coding this : No
 class Solution {
 public:
     double myPow(double x, int n) {
-        if(n==0) return 1;
-        if(n<0) {
-            n = abs(n);
-            x = 1/x;
-        }
+        if(n==0) 
+            return 1;
+        double temp = myPow(x,n/2);
         if(n%2==0){
-            return myPow(x*x, n/2);
+            return temp*temp;
         }
         else{
-            return x*myPow(x, n-1);
+            if(n<0) 
+                return temp*temp*(1/x);
+            else
+                return temp*temp*x;
         }
     }
 };
---
+++ problem2.cpp
@@ -0,0 +1,6 @@
+/*
+Time complexity: O(log(n))
+Space complexity: O(1)
+Did this code successfully run on Leetcode : Yes
+Any problem you faced while coding this : No
+*/
