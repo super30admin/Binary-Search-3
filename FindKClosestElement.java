8fb8be41dd222ac4573c4848902ab28a74f2264b
sohitrharma <rohit.sharma480@gmail.com>
2023-07-25 21:17:22 -0700

    Complete Binary-Search-3

--- FindKClosestElement.java
+++ FindKClosestElement.java
@@ -1,35 +1,29 @@
-// Time Complexity : O(n)
-// Space Complexity : O(n)
+// Time Complexity : O(log(n) + O(k))
+// Space Complexity : O(1)
 // Did this code successfully run on Leetcode : Yes
 // Any problem you faced while coding this : No
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.TreeSet;
-
-import javafx.util.Pair;
 
 class Solution {
     public List<Integer> findClosestElements(int[] arr, int k, int x) {
-        TreeSet<Pair<Integer, Integer>> set = new TreeSet<>((a, b) -> {
-            if (a.getValue().equals(b.getValue())) {
-                return a.getKey() - b.getKey();
-            }
-            return a.getValue() - b.getValue();
-        });
-
-        for (int num : arr) {
-            set.add(new Pair<>(num, Math.abs(x - num)));
-            if (set.size() > k) {
-                set.pollLast();
+        int low = 0;
+        int high = arr.length - k;
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            int distS = x - arr[mid];
+            int distE = arr[mid + k] - x;
+            if (distS > distE) {
+                low = mid + 1;
+            } else {
+                high = mid;
             }
         }
-
-        List<Integer> res = new ArrayList<>();
-        while (!set.isEmpty()) {
-            res.add(set.pollLast().getKey());
+        List<Integer> result = new ArrayList<>();
+        for (int i = low; i < low + k; i++) {
+            result.add(arr[i]);
         }
-
-        return res;
+        return result;
     }
 }
