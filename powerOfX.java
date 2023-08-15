8f5408b43e73073e26b91c332758f645dac2423b
rutujamandlikFT <123407038+rutujamandlikFT@users.noreply.github.com>
2023-08-15 19:47:03 -0400

    Create powerOfX.java

---
+++ powerOfX.java
@@ -0,0 +1,21 @@
+//T - O(log n)
+//S : O(1)
+
+class Solution {
+    public double myPow(double x, int n) {
+        //base
+        if(n == 0) return 1.00;
+        
+        //logic
+        double result = myPow(x, n/2);;
+        if(n%2 == 0){
+            return result * result;
+        }else{
+            if(n > 0){
+                return result * result * x;
+            }else{
+                return result * result * (1/x);
+            }
+        }
+    }
+}
