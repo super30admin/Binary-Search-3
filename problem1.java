a9cdba4c384d76df336732dde4bbee3f6f28ea6c
Saketh Reddy <sakethreddy@Sakeths-MacBook-Air.local>
2023-10-24 16:42:41 -0700

    p1 solved

---
+++ problem1.java
@@ -0,0 +1,33 @@
+/*
+
+Pow(x,n) (https://leetcode.com/problems/powx-n/)
+
+Time Complexity : O(log n)
+Space Complexity : O(log n)
+Did this code successfully run on Leetcode : Yes
+Any problem you faced while coding this : No
+
+*/
+
+
+class Solution {
+    public double myPow(double x, int n) {
+
+        if(n == 0){
+            return 1.0;
+        }
+
+        double result = myPow(x ,n/2);
+        if(n % 2 == 0){
+            return result * result;
+        }
+        else{
+            if(n > 0){
+                return result * result * x;
+            }
+           else{
+                return result * result * 1/x;
+           }
+        }
+    }
+}
