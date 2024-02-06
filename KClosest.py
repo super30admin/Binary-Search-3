6d4b1b4431904296ef72e6e98cb6db1eadee8df9
Humanvitha Sai Dharani Chinnam <humanvitha.2000@gmail.com>
2024-02-05 19:12:42 -0800

    updated closestK

--- KClosest.py
+++ KClosest.py
@@ -1,2 +1,21 @@
 # Problem2 Find K Closest Elements (https://leetcode.com/problems/find-k-closest-elements/)
 
+import heapq
+
+class Solution: 
+    def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
+        # using binary search
+        low = 0
+        high = len(arr) - k
+        while low < high:
+            mid = low + (high - low)//2
+            startDist = x - arr[mid]
+            endDist = arr[mid + k] - x
+            if startDist > endDist:
+                low = mid + 1
+            else:
+                high = mid
+        return arr[low : low + k]
+
+# Time complexity O(logn + k)
+# Space complexity O(1)
