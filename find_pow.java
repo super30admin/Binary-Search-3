c537dac783e75b6fa8ab11fb687733864b8f229b
Riyazmd007Riyazmd007Riyazmd007 <rm2883@nau.edu>
2024-03-07 01:54:17 -0700

    Binary search 3

---
+++ find_pow.java
@@ -0,0 +1,22 @@
+//TC: O(log(n))
+//SC: O(1)
+class Solution {
+    public double myPow(double x, int n) {
+       if(n==0)
+       return 1;
+        double result=myPow(x,n/2);
+        if(n%2==0)
+        {
+            return result*result;
+        }
+        else
+        {
+        if(n<0)
+       {
+       return result*result*1/x;
+       }
+        return result*result*x;
+        }
+        
+    }
+}
