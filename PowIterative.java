6f6d4f9bc4bcb8f4ffdf703e356e74dc0c61ad32
Amanjot Singh <amanjsingh@Amanjots-MacBook-Air.local>
2024-03-08 18:27:28 -0400

    Pow(x,n) - Iterative

---
+++ PowIterative.java
@@ -0,0 +1,25 @@
+// TC - O(log N)
+// SC - O(1)
+
+public class PowIterative {
+    class Solution {
+        public double myPow(double x, int n) {
+            double result = 1.0;
+
+            if (n < 0) {
+                n = n * (-1);
+                x = 1 / x;
+            }
+
+            while (n != 0) { // n>0 fails for -2147483648 -> No such positive value
+                if (n % 2 != 0) {
+                    result = result * x;
+                }
+                x = x * x;
+                n = n / 2;
+            }
+
+            return result;
+        }
+    }
+}
